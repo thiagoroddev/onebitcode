8a54daede35b0a5f5d69c1141cbe9fe865cb7484
thiagorod.dev <thiagorod.dev@gmail.com>
2023-11-05 15:29:35 -0300

    não sei

---
+++ JS III/eventos/aula-2.js
@@ -0,0 +1,19 @@
+function mouseEnter(){
+    console.log("Entrada do mouse")
+}
+
+function clickMouse() {
+    console.log("Click do mouse")
+}
+
+function focoMouse() {
+    console.log("Foco do mouse")
+}
+
+function desfoqueMouse() {
+    console.log("Desfoque do mouse")
+}
+
+function mudancaValor() {
+    console.log("Mudança de valor")
+}
