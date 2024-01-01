a2d009ff63e01e896e97cd1f8d39d4895c34cbd1
thiagorod.dev <thiagorod.dev@gmail.com>
2024-01-01 01:56:03 -0300

    finalizando..

--- JS III/jogo da velha/jogodavelha.js
+++ JS III/jogo da velha/jogodavelha.js
@@ -4,6 +4,8 @@ let novoJogo = document.getElementById('newGame')
 // referencia o nó pai do botão inicar jogo
 let playGame = document.getElementById('playGame')
 
+let container = document.getElementById('container')
+
 // Adiciona um ouvinte do evento tipo click ao botão 'Iniciar Jogo'
 let jogadorUm, jogadorDois, jogadorDaVez, jogadores, divJogadorUm, divJogadorDois, labelJogadorUm, labelJogadorDois, botaoIniciar, h3JogadorDaVez
 
@@ -15,17 +17,18 @@ novoJogo.addEventListener('click', function(eventoNovoJogo){
     jogadorUm.id = 'jogadorUm'
     jogadorUm.name = 'JodagorUm'
     jogadorUm.className = 'jogadores'
-    jogadorUm.placeholder = 'Nome do primeiro jogador'
-    jogadorUm.style.color = '#0004ff90'
+    jogadorUm.placeholder = 'Digite o nome 1º do jogador'
+   
     
     
     //adiciona label ao input
     labelJogadorUm = document.createElement('label')
     labelJogadorUm.htmlFor = 'jogadorUm'
-    labelJogadorUm.innerText = 'Jogador 1: '
+    labelJogadorUm.innerText = 'Jogador "X": '
     labelJogadorUm.id = 'labelJogadorUm'
-
-
+    labelJogadorUm.style.color = 'blue'
+    
+    
     // cria a div pai do input jogador um
     divJogadorUm = document.createElement('div')
     divJogadorUm.id = 'divJogadorUm'
@@ -36,103 +39,126 @@ novoJogo.addEventListener('click', function(eventoNovoJogo){
     
     // adiciona a div ao nó pai
     document.getElementById('playGame').appendChild(divJogadorUm)
-
-
+    
+    
     //Adiciona o input para o jogador dois
     jogadorDois = document.createElement('input')
     jogadorDois.type = 'text'
     jogadorDois.id = 'jogadorDois'
     jogadorDois.name = 'JodagorDois'
     jogadorDois.className = 'jogadores'
-    jogadorDois.placeholder = 'Nome do segundo jogador'
-    jogadorDois.style.color = '#ff000090'
-   
-
+    jogadorDois.placeholder = 'Digite o nome do 2º jogador '
+    
+    
     // adicionar label ao input
     labelJogadorDois = document.createElement('label')
     labelJogadorDois.htmlFor = 'jogadorDois'
-    labelJogadorDois.innerText = 'Jogador 2: '
+    labelJogadorDois.innerText = 'Jogador "O": '
     labelJogadorDois.id = 'labalJogadorDois'
-
+    labelJogadorDois.style.color = 'red'
+    
     divJogadorDois = document.createElement('div')
     divJogadorDois.id = 'divJogadorDois'
-
-
+    
+    
     // adiciona o label e input à div pai
     divJogadorDois.appendChild(labelJogadorDois)
     divJogadorDois.appendChild(jogadorDois)
-
+    
     // adiciona a div ao nó pai
     playGame.appendChild(divJogadorDois)
-
+    
     novoJogo.style.display = 'none'
-
+    
     // adicionar o botão iniciar jogo
     botaoIniciar = document.createElement('button')
     botaoIniciar.id = 'iniciar'
     botaoIniciar.innerText = 'Iniciar Jogo'
     
     playGame.appendChild(botaoIniciar)
-
+    
     // Adiciona um ouvinte de evento ao botão "Inicar Jogo"
-
+    
     botaoIniciar.addEventListener('click', function(iniciarJogo){
         if (jogadorUm.value === '' || jogadorDois.value === '') {
             alert('Preencha os nomes dos jogadores!')
         } else { 
-        divJogadorUm.style.display = 'none'
-        divJogadorDois.style.display = 'none'
-        botaoIniciar.style.display = 'none'
-        
-        //Mostra na tela o jogador da vez
-        h3JogadorDaVez = document.createElement('h3')
-        h3JogadorDaVez.innerText = 'Agora é a vez do jogador: '
-        h3JogadorDaVez.id = 'h3JogadorDaVez'
-    
-        jogadorDaVez = document.createElement('h2')
-        jogadorDaVez.innerText = jogadorUm.value
-        jogadorDaVez.id = 'jogadorDaVez'
-    
-        playGame.append(h3JogadorDaVez, jogadorDaVez)
-    
-        mudarJogador()
+            divJogadorUm.style.display = 'none'
+            divJogadorDois.style.display = 'none'
+            botaoIniciar.style.display = 'none'
+            
+            //Mostra na tela o jogador da vez
+            h3JogadorDaVez = document.createElement('h3')
+            h3JogadorDaVez.innerText = 'Agora é a vez do jogador: '
+            h3JogadorDaVez.id = 'h3JogadorDaVez'
+            
+            jogadorDaVez = document.createElement('h2')
+            jogadorDaVez.innerText = jogadorUm.value + ' : X'
+            jogadorDaVez.id = 'jogadorDaVez'
+            
+            playGame.append(h3JogadorDaVez, jogadorDaVez)
+            
+            mudarJogador()
+            
+            console.log('Jogador 1: ' + jogadorUm.value, ' \n Jogador 2: ' + jogadorDois.value,'\n Jogador da vez: ' + jogadorDaVez.textContent)
 
-        console.log('Jogador 1: ' + jogadorUm.value, ' \n Jogador 2: ' + jogadorDois.value,'\n Jogador da vez: ' + jogadorDaVez.textContent)
+
+            // Adiciona um botão para reiniciar a partida
+            botaoReiniciar = document.createElement('button')
+            botaoReiniciar.id = 'reiniciar'
+            botaoReiniciar.innerText = 'Reiniciar Rodadas'
+            botaoReiniciar.addEventListener('click', function(ev){   
+                location.reload();
+                
+            })
+
+            playGame.appendChild(botaoReiniciar)
+            
         } 
     }) 
 })
 
 // referencia as áreas que serão marcadas com X ou O
 let inputQuadrado = document.querySelectorAll('.quadrado')
-let vencedor = ''
 
 // Faz as marcações de X ou O no jogo e alterna a vez dos jogadores
+let vencedor = '' // O último a jogar
 function mudarJogador() {
     console.log(jogadorUm.value, jogadorDois.value, jogadorDaVez.textContent)
     
-
+    
     inputQuadrado.forEach(function(elemento) {
         elemento.addEventListener('click', function() {
-          //  Muda o valor do atributo 'value' quando o elemento é clicado
-            if (elemento.value === "" && jogadorDaVez.textContent == jogadorUm.value) {
-                elemento.value = "X";
-                jogadorDaVez.innerText = jogadorDois.value
-                vencedor = jogadorUm.value
-                console.log('Caixa ' + elemento.id + ' marcada pelo ' + vencedor)
-                
-             } if (elemento.value === "" && jogadorDaVez.textContent == jogadorDois.value) {
-                elemento.value = "O";
-                jogadorDaVez.innerText = jogadorUm.value
-                vencedor = jogadorDois.value
-                console.log('Caixa ' + elemento.id + ' marcada pelo ' + vencedor)
-            }
-
+            //  Muda o valor do atributo 'value' quando o elemento é clicado
+         if (elemento.value === "" && jogadorDaVez.textContent == jogadorUm.value+ ' : X') {
+            elemento.value = "X";
+            elemento.style.color = 'blue'
+            jogadorDaVez.innerText = jogadorDois.value + ' : O'
+            jogadorDaVez.style.color = 'red'
+            vencedor = jogadorUm.value
+            console.log('Caixa ' + elemento.id + ' marcada pelo ' + vencedor)
+            elemento.disabled = true
+            
+        } else if (elemento.value === "" && jogadorDaVez.textContent == jogadorDois.value+ ' : O') {
+            elemento.value = "O";
+            elemento.style.color = 'red'
+            jogadorDaVez.innerText = jogadorUm.value + ' : X'
+            jogadorDaVez.style.color = 'blue'
+            vencedor = jogadorDois.value
+            console.log('Caixa ' + elemento.id + ' marcada pelo ' + vencedor)
+            elemento.disabled = true
+        }     
             ganhador()
         });
     });
 }
 
 // Determina o ganhador
+let vitoriasJogador1 = 0
+let vitoriasJogador2 = 0
+let placarJogador1 = document.getElementById('placar-j1')
+let placarJogador2 = document.getElementById('placar-j2')
+
 function ganhador() {
     let A1 = document.getElementById('A1').value
     let A2 = document.getElementById('A2').value
@@ -144,18 +170,31 @@ function ganhador() {
     let C2 = document.getElementById('C2').value
     let C3 = document.getElementById('C3').value
     
-
+    
     if (A1 === 'X' && A2 === 'X' && A3 === 'X' || 
-        B1 === 'X' && B2 === 'X' && B3 === 'X' ||
-        C1 === 'X' && C2 === 'X' && C3 === 'X' ||
-        A1 === 'X' && B1 === 'X' && C1 === 'X' ||
-        A2 === 'X' && B2 === 'X' && C2 === 'X' ||
-        A3 === 'X' && B3 === 'X' && C3 === 'X' ||
-        A1 === 'X' && B2 === 'X' && C3 === 'X' ||
-        A3 === 'X' && B2 === 'X' && C1 === 'X'
-        ) { 
-        alert(`O jogador ${vencedor} ganhou!`)
-        limpar()
+    B1 === 'X' && B2 === 'X' && B3 === 'X' ||
+    C1 === 'X' && C2 === 'X' && C3 === 'X' ||
+    A1 === 'X' && B1 === 'X' && C1 === 'X' ||
+    A2 === 'X' && B2 === 'X' && C2 === 'X' ||
+    A3 === 'X' && B3 === 'X' && C3 === 'X' ||
+    A1 === 'X' && B2 === 'X' && C3 === 'X' ||
+    A3 === 'X' && B2 === 'X' && C1 === 'X') { 
+        vitoriasJogador1++
+        placarJogador1.innerText = vitoriasJogador1
+        inputQuadrado.forEach(function(elemento){
+           if (elemento.value === 'X'){
+               elemento.style.backgroundColor = 'blue'
+               elemento.style.color = 'white'
+           }
+        })
+        alert(`O jogador ${vencedor} ganhou!`);
+
+        setTimeout(function() {
+            limpar()
+          }, 1000);
+        
+        
+        
     } else if (
         A1 === 'O' && A2 === 'O' && A3 === 'O' || 
         B1 === 'O' && B2 === 'O' && B3 === 'O' ||
@@ -164,25 +203,40 @@ function ganhador() {
         A2 === 'O' && B2 === 'O' && C2 === 'O' ||
         A3 === 'O' && B3 === 'O' && C3 === 'O' ||
         A1 === 'O' && B2 === 'O' && C3 === 'O' ||
-        A3 === 'O' && B2 === 'O' && C1 === 'O'
-    ) { 
-        alert(`O jogador ${vencedor} ganhou!`)
-        limpar()
+        A3 === 'O' && B2 === 'O' && C1 === 'O') { 
+            vitoriasJogador2 ++
+            placarJogador2.innerText = vitoriasJogador2
+
+            alert(`O jogador ${vencedor} ganhou!`)
+            limpar()
+            
+        } else if (A1 && A2 && A3 && B1 && B2 && B3 && C1 && C2 && C3) { 
+            alert('EMPATOU!')
+            
+            limpar()
+        }  
+       
     }
-}
+    
+    
+    // Limpa o jogo 
+    function limpar() {
+        inputQuadrado.forEach(function(elemento) { 
+            elemento.value = ''
+            elemento.disabled = false
+            elemento.style.backgroundColor = 'white'
+        })
+    }
 
-// Limpa o jogo 
-function limpar() {
-    inputQuadrado.forEach(function(elemento) { 
-        elemento.value = ''
-    })
-}
-
-
-
-
-
-
-
-
-
+   
+   
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
