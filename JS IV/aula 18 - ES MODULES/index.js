f5a7ec3c0f8ed6612e0078f93892f4937377f3bc
thiagorod.dev <thiagorod.dev@gmail.com>
2024-01-15 20:26:18 -0300

    aula 18

---
+++ JS IV/aula 18 - ES MODULES/index.js
@@ -0,0 +1,10 @@
+import { inline } from "./inline.js"
+import padraoNaoPrecisaDeNome from "./inline.js"
+import exportDefault, { group, a, b, c, d } from "./non-inline.js"
+
+inline()
+padraoNaoPrecisaDeNome()
+
+exportDefault()
+group()
+
